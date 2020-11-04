e54051f4596f1697acff6639886a5ba2fdc16e70
Rodrigo <rodikinchoku@gmail.com>
2020-11-04 17:52:39 -0300

    Menu concertado

--- src/Components/Menu/index.js
+++ src/Components/Menu/index.js
@@ -3,7 +3,7 @@ import { MenuItems } from "./MenuItems";
 import { Button } from "./Button";
 import "./style.css";
 
-class Menu extends Component {
+/*class Menu extends Component {
   
     //Variáveis 
 
@@ -41,6 +41,25 @@ class Menu extends Component {
 
     
 
+}*/
+
+const Menu = () => {
+    return( 
+        <nav class="navbar fixed-top navbar-expand-lg navbar-light" style={{backgroundColor: "lightblue"}}>
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="#">Features</a>
+                    <a class="nav-item nav-link" href="#">Pricing</a>
+                    <a class="nav-item nav-link disabled" href="#">Disabled</a>
+                </div>
+            </div>
+        </nav>
+    );
 }
 
 export default Menu;
