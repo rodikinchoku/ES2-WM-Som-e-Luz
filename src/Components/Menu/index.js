56a0cff38d0d9ebb944b5e9d8a26afe4e3ee94fd
Rodrigo <rodikinchoku@gmail.com>
2020-11-05 02:00:22 -0300

    Menu quase finalizado

--- src/Components/Menu/index.js
+++ src/Components/Menu/index.js
@@ -1,64 +1,30 @@
 import React, { Component } from 'react';
-import { MenuItems } from "./MenuItems";
-import { Button } from "./Button";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSignOutAlt }  from "@fortawesome/free-solid-svg-icons";
+
 import "./style.css";
-
-/*class Menu extends Component {
-  
-    //Variáveis 
-
-    state = { clicked: false}
-
-    //Funções 
-
-    handleClick = () => {
-        this.setState({ clicked: !this.state.clicked })
-    }
-
-
-    render() {
-        return (
-            <nav className="NavbarItems">
-                <h1 className="navbar-logo">React<i className="fab fa-react"></i></h1>
-                <div className="menu-icon" onClick={this.handleClick}>
-                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
-                </div>
-                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
-                    {MenuItems.map((item, index) => {
-                        return (
-                            <li key={index}>
-                                <a className={item.cName} href={item.url}>
-                                    {item.title}
-                                </a>
-                            </li>
-                        )
-                    })}
-                </ul>
-                <Button>Sign Up</Button>
-            </nav>
-        )
-    }
-
-    
-
-}*/
 
 const Menu = () => {
     return( 
-        <nav class="navbar fixed-top navbar-expand-lg navbar-light" style={{backgroundColor: "lightblue"}}>
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-                    <a class="nav-item nav-link" href="#">Features</a>
-                    <a class="nav-item nav-link" href="#">Pricing</a>
-                    <a class="nav-item nav-link disabled" href="#">Disabled</a>
-                </div>
-            </div>
-        </nav>
+        <Navbar expand="lg" className="menu" >
+        <Navbar.Brand className="logo">Logo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto menuItems">
+            <Nav.Link href="#home"><span className="text">Agenda</span></Nav.Link>
+            <Nav.Link href="#link"><span className="text">Galeria de Fotos</span></Nav.Link>
+            <Nav.Link href="#link"><span className="text">Serviços</span></Nav.Link>
+          </Nav>
+          <Button className="logoutButton" variant="outline-primary">
+            <span className="text">Logout</span>
+            <FontAwesomeIcon icon={faSignOutAlt} />
+          </Button>
+        </Navbar.Collapse>
+       
+      </Navbar>
     );
 }
 
